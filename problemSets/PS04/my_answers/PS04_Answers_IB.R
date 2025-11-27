4edf3bdc0ec30dbba536efedb03c79685eec67b6
Ivy-B1rch <birchali@tcd.ie>
2025-11-27 21:11:13 +0000

    Answers for ProblemSet 4
    Answers!

--- problemSets/PS04/my_answers/PS04_Answers_IB.R
+++ problemSets/PS04/my_answers/PS04_Answers_IB.R
@@ -25,7 +25,59 @@ pkgTest <- function(pkg){
 
 # here is where you load any necessary packages
 # ex: stringr
-lapply(c("car"),  pkgTest)
 
+lapply(c("car"),  pkgTest)
+library(car)
+library(dplyr)
+data(Prestige) #note to self: run this whenever you need to reset the dataframe
+help(Prestige)
+
+
+Prestige
 # set wd for current folder
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
+
+
+Prestige$type
+
+#part 1
+
+#using some stuff i learned in Comp Sci for Recoding without needing to use ifelse
+professional <- recode(Prestige$type, 'prof'=1, 'bc'=0, 'wc' = 0)
+Prestige <- cbind(Prestige, professional)
+
+Prestige
+
+#outcome, continuous, dummy, continuous*Dummy
+model = lm(prestige~income + professional + income*professional, data = Prestige)
+
+summary(model)
+
+#just so I don't have to do math, question 1.f
+(0.0031709*1000 + 37.7812800*1 + 1000*1*0.0023257)-(0.0031709*0 + 37.7812800*1 + 0*1*0.0023257)
+
+#1.g
+(0.0031709*6000 + 37.7812800*1 + 6000*1*0.0023257)-(0.0031709*6000 + 37.7812800*0 + 6000*0*0.0023257)
+
+
+#2a
+ts <- 0.042/0.016
+ts
+
+#130-2 is n-k, n is 130 and k, the number of variables, is 2 
+p_value <- 2*pt(abs(ts),130-2,lower.tail = F) 
+p_value
+
+
+
+#2b
+ts2 <- 0.042/0.013
+ts2
+
+p_value2 <- 2*pt(abs(ts2),130-2,lower.tail = F) 
+p_value2
+
+
+
+#2d
